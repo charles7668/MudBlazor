0eb8e814051e3493d3b82da0064bb118a5b1827e
charles7668 <charles76680119@gmail.com>
2024-12-01 13:12:34 +0800

    MudTextField: Align the display style of AutoGrow=true with that of AutoGrow=false.

--- src/MudBlazor.UnitTests/Components/TextFieldTests.cs
+++ src/MudBlazor.UnitTests/Components/TextFieldTests.cs
@@ -1672,6 +1672,16 @@ namespace MudBlazor.UnitTests.Components
             comp.SetParametersAndRender(p => p.Add(x => x.ReadOnly, true)); //no clear button when readonly
             comp.FindAll(".mud-input-clear-button").Count.Should().Be(0);
         }
+
+        [Test]
+        public void TestAutoGrowPageTest()
+        {
+            var comp = Context.RenderComponent<TextFieldAutoGrowTest>();
+            comp.FindAll("input.mud-input-root-filled").Count.Should().Be(4);
+            comp.FindAll("textarea.mud-input-root-filled").Count.Should().Be(4);
+            comp.FindAll("input.mud-input-root-filled.mud-input-root-margin-dense").Count.Should().Be(2);
+            comp.FindAll("textarea.mud-input-root-filled.mud-input-root-margin-dense").Count.Should().Be(2);
+        }
 #nullable disable
     }
 }
